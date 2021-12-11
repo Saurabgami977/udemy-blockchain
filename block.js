2445b6e8e80f2e088d41c0639c5e03a1120a356e
saurab <saurab@saurabgami.com>
2021-12-11 16:58:41 +0545

    adjust the difficulty

--- block.js
+++ block.js
@@ -16,15 +16,15 @@ class Block {
 	}
 
 	static mineBlock({ lastBlock, data }) {
-		let hash, timestamp
-		// const timestamp = Date.now();
 		const lastHash = lastBlock.hash;
-		const { difficulty } = lastBlock
+		let hash, timestamp
+		let { difficulty } = lastBlock
 		let nonce = 0;
 
 		do {
 			nonce++;
 			timestamp = Date.now();
+			difficulty = Block.adjustDifficulty({ originalBlock: lastBlock, timestamp })
 			hash = cryptoHash(timestamp, lastHash, data, nonce, difficulty)
 		} while (hash.substring(0, difficulty) !== '0'.repeat(difficulty))
 
@@ -35,6 +35,8 @@ class Block {
 
 	static adjustDifficulty({ originalBlock, timestamp }) {
 		const { difficulty } = originalBlock
+
+		if (difficulty < 1) return 1
 
 		const difference = timestamp - originalBlock.timestamp;
 
