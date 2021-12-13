3a2e6748bb23e64fec0361500f260cb1d3abf768
Saurab Gami <saurabgami977@gmail.com>
2021-12-13 10:41:43 +0545

    implementing redis pub/sub for updating peers

---
+++ pubsub.js
@@ -0,0 +1,26 @@
+const redis = require("redis");
+
+const CHANNEL = {
+	TEST: "TEST",
+};
+
+class PubSub {
+	constructor() {
+		this.publisher = redis.createClient();
+		this.subscriber = redis.createClient();
+
+		this.subscriber.subscribe(CHANNEL.TEST);
+
+		this.subscriber.on("message", (channel, message) =>
+			this.handleMessage(channel, message),
+		);
+	}
+
+	handleMessage = (channel, message) => {
+		console.log(`Message received. Channel: ${channel}, message: ${message}`);
+	};
+}
+
+const testPubSub = new PubSub();
+
+setTimeout(() => testPubSub.publisher.publish(CHANNEL.TEST, "foo"), 1000);
