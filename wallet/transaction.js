629f381278093c6df6ebc64e4ac20a954400fd32
Saurab Gami <saurabgami977@gmail.com>
2022-05-14 14:56:38 +0545

    validate transaction

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -1,4 +1,5 @@
 const uuid = require("uuid/v1");
+const { verifySignature } = require("../utils");
 
 class Transaction {
 	constructor({ senderWallet, recipient, amount }) {
@@ -23,6 +24,29 @@ class Transaction {
 			address: senderWallet.publicKey,
 			signature: senderWallet.sign(outputMap),
 		};
+	}
+
+	static validTransaction(transaction) {
+		const {
+			input: { address, amount, signature },
+			outputMap,
+		} = transaction;
+
+		const outputTotal = Object.values(outputMap).reduce(
+			(total, outputAmount) => total + outputAmount,
+		);
+
+		if (amount !== outputTotal) {
+			console.error(`Invalid transaction from ${address}`);
+			return false;
+		}
+
+		if (!verifySignature({ publicKey: address, data: outputMap, signature })) {
+			console.error(`Invalid transaction from ${address}`);
+			return false;
+		}
+
+		return true;
 	}
 }
 
