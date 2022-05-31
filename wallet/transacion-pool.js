107e12a43be496da24c850f9af392ef78a158503
Saurab Gami <saurabgami977@gmail.com>
2022-05-31 15:16:25 +0545

    transaction update in the api

--- wallet/transacion-pool.js
+++ wallet/transacion-pool.js
@@ -6,6 +6,14 @@ class TransactionPool {
 	setTransaction(transaction) {
 		this.transactionMap[transaction.id] = transaction;
 	}
+
+	existingTransaction({ inputAddress }) {
+		const transaction = Object.values(this.transactionMap);
+
+		return transaction.find(
+			(transaction) => transaction.input.address === inputAddress,
+		);
+	}
 }
 
 module.exports = TransactionPool;
