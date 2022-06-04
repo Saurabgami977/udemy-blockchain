b158cbc0a075b4adede0ecc762b59c2c5bcc78b9
Saurab Gami <saurabgami977@gmail.com>
2022-06-04 12:12:25 +0545

    calculate the balance before each transaction

--- wallet/index.js
+++ wallet/index.js
@@ -15,7 +15,13 @@ class Wallet {
 		return this.keypair.sign(cryptoHash(data));
 	}
 
-	createTransaction({ recipient, amount }) {
+	createTransaction({ recipient, amount, chain }) {
+		if (chain) {
+			this.balance = Wallet.calculateBalance({
+				chain,
+				address: this.publicKey,
+			});
+		}
 		if (amount > this.balance) {
 			throw new Error("Amount exceeds balance");
 		}
